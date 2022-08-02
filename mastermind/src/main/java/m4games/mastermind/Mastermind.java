8b758a24bdd0d340d855944b65a8db07d8f08842
IzanDuesoLopez <izandueso@gmail.com>
2022-08-02 09:16:36 +0200

    MainApp y Mastermind class

---
+++ mastermind/src/main/java/m4games/mastermind/Mastermind.java
@@ -0,0 +1,21 @@
+package m4games.mastermind;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Mastermind extends JFrame{
+	
+	public Mastermind() {
+		
+	}
+	
+	// Vector de colores
+	Color[] colores =  { Color.RED, Color.RED, Color.GREEN, Color.GREEN ,Color.BLUE, Color.BLUE, Color.BLACK, Color.BLACK };
+	Color[] coloresDisponibles = new Color[4];
+	
+	// Método crear_colores. Asignamos colores aleatorios al array de 4 posiciones.
+	
+	/* Método crear_solución. Obtener aleatoriamente colores para la solución. Estos colores los guardamos
+	   en un vector aux. */
+}

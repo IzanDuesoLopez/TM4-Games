6c6f52eb56963956d2fa121c5a97223e620faa5c
daniflorialopez <daniflorialopez@gmail.com>
2022-08-04 11:50:41 +0200

    Quitar imports

--- mastermind/src/main/java/m4games/mastermind/JuegoMainApp.java
+++ mastermind/src/main/java/m4games/mastermind/JuegoMainApp.java
@@ -1,19 +1,6 @@
 package m4games.mastermind;
 
 import java.awt.EventQueue;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowEvent;
-
-import javax.swing.BorderFactory;
-import javax.swing.JLabel;
-import javax.swing.border.Border;
-
-import java.awt.Font;
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Label;
-import javax.swing.border.LineBorder;
 
 public class JuegoMainApp {
 
